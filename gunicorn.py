17b7b22423b0ec04c6a3f23cc7f3c686c76ced47
Chris Latham <backwardspy@gmail.com>
2020-05-05 15:31:45 +0100

    fix style issue

--- gunicorn.py
+++ gunicorn.py
@@ -4,5 +4,5 @@ import multiprocessing
 bind = f":{getenv('GUNICORN_PORT', 9000)}"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = multiprocessing.cpu_count()
-accesslog = '-'
-errorlog = '-'
+accesslog = "-"
+errorlog = "-"
